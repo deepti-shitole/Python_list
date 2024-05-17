d3810eed5e3d22c1a3a3e0ee96f354661e717e5e
Deepti <93430410+deepti-shitole@users.noreply.github.com>
2024-05-17 17:03:43 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+import os
+# Define the two lists
+list1 = [1, 2, 3, 4, 5]
+list2 = [5, 4, 3, 2, 1]
+
+# Reverse the first list
+list1.reverse()
+
+# Sort the second list
+list2.sort()
+
+# Print the modified lists
+print("Reversed list:", list1)
+print("Sorted list:", list2)
